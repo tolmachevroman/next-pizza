ab05971b2cc94e4bd792ff29da722575b3142802
Roman Tolmachev <tolmachevroman@gmail.com>
2024-08-17 17:12:08 -0500

    add Prisma integration

---
+++ app/api/users/route.ts
@@ -0,0 +1,13 @@
+import { prisma } from '@/prisma/prisma-client';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json({ users: users });
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+  const user = await prisma.user.create({ data });
+  return NextResponse.json({ user });
+}
